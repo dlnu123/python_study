32fec7a1f95a6244d401816df599407a692202ed
王亮 <hbxjwl@yeah.net>
2019-08-25 12:02:04 +0800

    高阶函数sorted学习

---
+++ com/maoyan/study/functional/higher_order/higher_order4_sorted.py
@@ -0,0 +1,28 @@
+# sorted高阶函数学习
+
+print(sorted([36, 5, -12, 9, -21]))
+print(sorted([36, 5, -12, 9, -21], key=abs))
+print(sorted(['bob', 'about', 'Zoo', 'Credit']))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=False))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True))
+
+
+# 课后习题
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+
+def by_name(t):
+    return t[0]
+
+
+L2 = sorted(L, key=by_name)
+print(L2)
+
+
+def by_score(t):
+    return t[1]
+
+
+L2 = sorted(L, key=by_score, reverse=True)
+print(L2)
