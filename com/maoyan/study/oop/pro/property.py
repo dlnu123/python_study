37654909986861c0f24c28ae8a7477af4783ee86
wangliang30 <wangliang30@maoyan.com>
2019-08-27 19:04:53 +0800

    property学习

---
+++ com/maoyan/study/oop/pro/property.py
@@ -0,0 +1,54 @@
+class Student(object):
+
+    @property
+    def score(self):
+        return self.__score
+
+    @score.setter
+    def score(self, score):
+        if not isinstance(score, int):
+            raise ValueError('score must be an integer!')
+        if score < 0 or score > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self.__score = score
+
+
+s = Student()
+s.score = 60
+print(s.score)
+# s.score = 900
+
+
+# 课后习题
+class Screen(object):
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def resolution(self):
+        return self.__width * self.__height
+
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
