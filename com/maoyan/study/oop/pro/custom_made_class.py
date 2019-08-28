353f2f8b340d4726d776e36b51704c677360ebba
wangliang30 <wangliang30@maoyan.com>
2019-08-28 17:29:27 +0800

    定制类+多重继承

---
+++ com/maoyan/study/oop/pro/custom_made_class.py
@@ -0,0 +1,14 @@
+# __slots__
+# __len__()
+# __str__()、__repr__()
+# __iter__()、__next__()
+# __getitem__()、__setitem__()、__delitem__()
+# __getattr__()
+# __call__()
+
+
+# 1、__str__()、__repr__()-------打印对象内容
+# 2、__iter__()、__next__()------斐波那契数列打印
+# 3、__getitem__()、__setitem__()、__delitem__()-------斐波那契数列下标、切片（正负数、步长）实现
+# 4、__getattr__()
+# 5、__call__()、callable()-----使对象可以被调用
