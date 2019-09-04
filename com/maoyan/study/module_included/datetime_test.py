59b92b9f10275ca643b6ff4453ba321adc3976a3
王亮 <hbxjwl@yeah.net>
2019-09-04 22:53:37 +0800

    datetime习题参考答案提交

--- com/maoyan/study/module_included/datetime_test.py
+++ com/maoyan/study/module_included/datetime_test.py
@@ -9,6 +9,8 @@
 # 8、本地时间转换为UTC时间
 # 9、时区转换
 
+# 时区是弱项
+
 import datetime
 
 
@@ -62,7 +64,18 @@ from datetime import datetime, timezone, timedelta
 
 
 def to_timestamp(dt_str, tz_str):
-    pass
+    # strptime：将字符串时间转换为 datetime
+    dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+    # 正则获取 时差字符串
+    m = re.match(r'^(UTC)(.*?)(\:.*)$', tz_str)
+    # 将字符串转换为数字
+    n = int(m.group(2))
+    # 创建时区
+    tz_utc_n = timezone(timedelta(hours=n))
+    # 转换为 UTC 时间
+    utc_dt = dt.replace(tzinfo=tz_utc_n)
+    # 将时间转换为 timestamp 并返回
+    return utc_dt.timestamp()
 
 
 # 测试:
