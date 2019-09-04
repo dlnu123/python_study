d5ea48a4844cb6005c595b8cc7b1d69407b65c02
wangliang30 <wangliang30@maoyan.com>
2019-09-04 11:17:21 +0800

    默认参数测试

--- com/maoyan/study/basic/basic1.py
+++ com/maoyan/study/basic/basic1.py
@@ -12,11 +12,22 @@
 # else:
 #     print(-a)
 
-from functools import reduce
+# from functools import reduce
+#
+# sp = "123.456".split('.')
+# print(reduce(lambda x, y: x * 10 + y, map(int, sp[0])))
+# print(reduce(lambda x, y: x * 10 + y, map(int, sp[1])) / 10**len(sp[1]))
+#
+# s = ''
+# print(s and s.strip())
 
-sp = "123.456".split('.')
-print(reduce(lambda x, y: x * 10 + y, map(int, sp[0])))
-print(reduce(lambda x, y: x * 10 + y, map(int, sp[1])) / 10**len(sp[1]))
 
-s = ''
-print(s and s.strip())
+# 函数的默认参数，在使用的时候，可以不考虑顺序问题
+def test(a, b=1, c=2):
+    print(a, b, c)
+
+
+test(1)
+test(1, c=100)
+test(1, b=200, c=100)
+test(12, c=34, b=56)
